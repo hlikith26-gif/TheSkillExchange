8071fd6362b19b62135f0a3560b657b89bf4286d
hlikith26-gif <hlikith26@gmail.com>
2026-09-01 20:47:21 +0530

    javascript

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const emailInput = document.getElementById("Email");
 const passwordInput = document.getElementById("password");
 const client = new Appwrite.Client();
+
+document.getElementById("signup").addEventListener("click", async () => {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    if (!email || !password) {
+        console.log("enter email or password")
+        return;
+    }
+    try {
+        
+    }
+}
