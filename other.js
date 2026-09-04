e3cf7cc26b66dde1285baa1c47f7cdce2c8c45d0
hlikith26-gif <hlikith26@gmail.com>
2026-09-04 15:25:35 +0530

    a lot more work on main.html

--- other.js
+++ other.js
@@ -13,3 +13,24 @@ links.forEach(link => {
     }
   });
 });
+// Above code is AI Generated 
+
+//Below code is NOT AI Generated
+
+const feedback_pop = document.getElementById("feedback-popup")
+
+function feedback_show() {
+    feedback_pop.showModal()
+}
+function close_feedback() {
+    feedback_pop.close()
+}
+
+const publish_pop = document.getElementById("publish-popup")
+
+function publish_show() {
+    publish_pop.showModal()
+}
+function close_publish() {
+    publish_pop.close()
+}
