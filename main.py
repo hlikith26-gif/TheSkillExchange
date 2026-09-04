f88ef914eecfc61cea0584243e6c456b77f33c12
Particle123456 <nainasatish@gmail.com>
2026-09-04 17:01:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 from pyscript import document
+import random
+def randomize():
+    subjects = {    
+    "1":"Mathematics"
+    "2":"Physics"
+    "3":"Chemistry"
+    "4":"Biology"
+    "5":"History"
+    "6":"Political Sciences"
+    "7":"Geography"
+    "8":"Law"
+    "9":"Computer Science"
+    "10":"Financial Literacy"
+    "11":"Indian Languages"
+    "12":"Foreign Languages"
+    "13":"others"
+    }
+
+    
+
+
