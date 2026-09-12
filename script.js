66b7cddf6b2895e73e474a814fc5755328f63b77
hlikith26-gif <hlikith26@gmail.com>
2026-09-12 19:23:11 +0530

    push

--- script.js
+++ script.js
@@ -121,12 +121,8 @@ async function AddEvent() {
             ldtr: ldate,
             doe: edate
         })
-    if (error.message == 'new row violates row-level security policy for table "events"') {
-        alert("You need to login")
-        window.location.href = "login.html";
-        return;
     
-    } else if (error) {
+    if (error) {
         alert(error.message)
         return;
     } else {
@@ -283,3 +279,4 @@ async function getName() {
     const user = data.user
     document.getElementById("nameDis").innerHTML = user.user_metadata.name
 }
+getName()
